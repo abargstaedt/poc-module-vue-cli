fcfc3d9155cf7da19072600bee3e78e0e1857bba
abargstaedt <andreas.bargstaedt@fielmann.com>
2019-11-27 16:17:10 +0100

    Remove console output

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-/* eslint-disable no-console */
 import Vue from 'vue';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
 
 export function getComponent() {
-  console.log('diHTMl module is using Vue version:', Vue.version);
   const component = new Vue({
     render: h => h(App)
   }).$mount();
